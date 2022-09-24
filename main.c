d55d59acf9d659cea9301412e799540bed8d81fd
INVISÍVEL <>
2022-09-24 02:03:42 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Joanice Reeis\n");
+  printf("%c%c%cPrimeiro programa", '\n', '\t', '\"');
+  printf("%c\n", '\"');
+  return 0;
+}
